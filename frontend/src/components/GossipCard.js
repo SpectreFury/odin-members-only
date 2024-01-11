7d4d36d8d403de55209105e202549d0f332b1113
Ayush Soni <soni.ayush.2212@gmail.com>
2024-01-11 21:03:22 +0530

    Add dashboard

---
+++ frontend/src/components/GossipCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const GossipCard = () => {
+  return (
+    <div className="flex flex-col max-w-[600px] p-2 bg-slate-200 my-2">
+      <h1>
+        Spotted: Georgina in a coffee shop with Serena Van Der Woodsen. What are
+        this two girls doing together? Is Dan aware that his wife is with his
+        ex, and formerly best friend?
+      </h1>
+      <div>By: Anonymous</div>
+    </div>
+  );
+};
+
+export default GossipCard;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GossipCard from "../components/GossipCard";
+
+const Home = () => {
+  return (
+    <main className="flex flex-col items-center">
+      <GossipCard />
+    </main>
+  );
+};
+
+export default Home;
